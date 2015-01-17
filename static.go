6f53771342ae282570c6aafc3f817ba85eec2115
Tim Miller <tim.miller.0@gmail.com>
2015-01-17 03:34:45 -0800

    log requests.

--- static.go
+++ static.go
@@ -9,8 +9,15 @@ import (
 var bind = flag.String("bind", ":18181", "bind address")
 var root = flag.String("root", "/srv/http", "root directory")
 
+func loggingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	flag.Parse()
-	http.Handle("/", http.FileServer(http.Dir(*root)))
+	http.Handle("/", loggingHandler(http.FileServer(http.Dir(*root))))
 	log.Fatal(http.ListenAndServe(*bind, nil))
 }
