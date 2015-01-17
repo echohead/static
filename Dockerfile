5658b2b7af17b9ddbc8cddd92db31e0fc2cd5b9e
Tim Miller <tim.miller.0@gmail.com>
2015-01-17 04:34:49 -0800

    add a dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM echohead/scratch
+ADD static static
+USER nobody
+EXPOSE 8080
--- static.go
+++ static.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 )
 
-var bind = flag.String("bind", ":18181", "bind address")
-var root = flag.String("root", "/srv/http", "root directory")
+var bind = flag.String("bind", ":8080", "bind address")
+var root = flag.String("root", "/srv", "root directory")
 
 func loggingHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -19,5 +19,6 @@ func loggingHandler(h http.Handler) http.Handler {
 func main() {
 	flag.Parse()
 	http.Handle("/", loggingHandler(http.FileServer(http.Dir(*root))))
+	log.Printf("starting on %v", *bind)
 	log.Fatal(http.ListenAndServe(*bind, nil))
 }
