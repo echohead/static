fc220844d99877a7e0b20017d985c995804cbb71
Tim Miller <tim.miller.0@gmail.com>
2015-08-06 11:25:56 -0700

    require --root flag.

--- static.go
+++ static.go
@@ -7,7 +7,7 @@ import (
 )
 
 var bind = flag.String("bind", ":8080", "bind address")
-var root = flag.String("root", "/srv", "root directory")
+var root = flag.String("root", "", "root directory")
 
 func loggingHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +18,10 @@ func loggingHandler(h http.Handler) http.Handler {
 
 func main() {
 	flag.Parse()
+	if *root == "" {
+		log.Fatal("missing required flag --root")
+	}
 	http.Handle("/", loggingHandler(http.FileServer(http.Dir(*root))))
-	log.Printf("starting on %v", *bind)
+	log.Printf("serving %v on %v", *root, *bind)
 	log.Fatal(http.ListenAndServe(*bind, nil))
 }
